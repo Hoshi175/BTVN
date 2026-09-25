66dd45c29fba3f39d30354c8a3f8f77a73fc54fb
Hoshi175 <khangbvn58@gmail.com>
2026-09-25 22:48:52 +0700

    Update Bài 1.cpp

--- Bài 1.cpp
+++ Bài 1.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-int count_digits (int number) //khai báo của hàm
+int count_digits (int number)    //khai báo của hàm
 {
     int digits = 0;
 
     while (number != 0)
     {
         number = number / 10;
-        digits++; //tăng bộ đếm thêm 1
+        digits++;    //tăng bộ đếm thêm 1
     }
     return digits;
 }
